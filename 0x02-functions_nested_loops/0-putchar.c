bb609763ee264755e00fc52c8cf18f33118004ae
ebenezeroye <eboyedokun@gmail.com>
2022-07-06 10:01:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,11 @@
 /**
  * main - Print the word "_putchar"
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf ("_putchar\n");
+	printf("_putchar\n");
 	return (0);
 }
